3266c3a7e83df29d4ebbb86a8b5993468e9418a4
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2021-10-20 11:35:16 +0400

    Update Orc.js

--- src/Orc.js
+++ src/Orc.js
@@ -55,7 +55,7 @@ const lookupsOrc = async ()=>{
 
  },[tokenid])
 
- let additionalClasses = "bg-yellow-50 border-yellow-600 border-2 p-2 my-2"
+ let additionalClasses = "bg-yellow-50 border-yellow-600 border-2 p-4 my-2"
  let baseClass = "p-2"
 
  if(allData){

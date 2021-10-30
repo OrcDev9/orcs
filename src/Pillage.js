06d95f3908393277c0b6e6d243ced603ffc099c1
cryptoafrican <71972487+cryptoafrican@users.noreply.github.com>
2021-10-30 09:01:53 +0200

    Update Pillage.js

--- src/Pillage.js
+++ src/Pillage.js
@@ -18,13 +18,13 @@ import { getDatabase, ref, set, onValue, query, get,child, equalTo, orderByValue
 const places = [
     {place: "TOWN", level:1, image:town, index:0},
     {place: "DUNGEON", level:3, image:dungeon, index:1},
-    {place: "CRYPT", level:6, image:crypt, index:2},
-    {place: "CASTLE", level:15, image:castle, index:3},
-    {place: "DRAGON'S LAIR", level:25, image:dragon, index:4},
-    {place: "THE ETHER", level:36, image:ether, index:5},
-    {place: "TAINTED KINGDOM", level:15, image:kingdom, index:6},
-    {place: "OOZING DEN", level:25, image:den, index:7},
-    {place: "ANCIENT CHAMBER", level:45, image:chamber, index:8},
+ //   {place: "CRYPT", level:6, image:crypt, index:2},
+ //   {place: "CASTLE", level:15, image:castle, index:3},
+ //   {place: "DRAGON'S LAIR", level:25, image:dragon, index:4},
+ //   {place: "THE ETHER", level:36, image:ether, index:5},
+ //   {place: "TAINTED KINGDOM", level:15, image:kingdom, index:6},
+ //   {place: "OOZING DEN", level:25, image:den, index:7},
+ //   {place: "ANCIENT CHAMBER", level:45, image:chamber, index:8},
   //  {place: "ORC GODS", level:52, image:gods, index:9},
    ]
 

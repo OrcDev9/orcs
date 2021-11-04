bfd94f33f16a6868d9fab5c743d6bbd781a0624e
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2021-11-04 22:29:36 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,6 +56,9 @@ function App() {
                 <Route path="/admin">  
                       {flip ? ( <>
                       <Horde contract={nftContract} />
+                      <br/>
+                      Experimental - Dont use.
+                      <br/>
                       <GetAllOrcMetaData />
                       <OrcImages />
 

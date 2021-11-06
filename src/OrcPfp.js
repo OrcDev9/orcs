83d98d3cb8008ea0f6b0909eb015806d892fe479
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2021-11-06 21:54:16 +0400

    s

--- src/OrcPfp.js
+++ src/OrcPfp.js
@@ -16,24 +16,26 @@ function OrcPFP({orc}) {
     const orcLookupRef = useRef();
   //  <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`data:image/png;base64,${m}`}/>
   //  <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`data:image/png;base64,${o}`}/>
-  const ref = useRef()
+  const ref0 = useRef()
+  const ref1 = useRef()
+  const ref2 = useRef()
 
-  const onButtonClick = useCallback(() => {
-    if (ref.current === null) {
-      return
-    }
+  const onButtonClick = (refIndex)=>{
+    let refs = [ref0, ref1, ref2]
+  console.log(refIndex, refs[refIndex].current)
 
-    toPng(ref.current, { cacheBust: true, })
-      .then((dataUrl) => {
-        const link = document.createElement('a')
-        link.download = `Orcpfp.png`
-        link.href = dataUrl
-        link.click()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [ref])
+    toPng(refs[refIndex].current, { cacheBust: true, })
+    .then((dataUrl) => {
+      const link = document.createElement('a')
+      link.download = `Orcpfp.png`
+      link.href = dataUrl
+      link.click()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  }
 
   const handleOrcSubmit = async (e) => {
       
@@ -76,7 +78,10 @@ function OrcPFP({orc}) {
 
 return (
     <>
+    <div class="text-3xl">
     <Title text={"ORC PFP"} />
+    </div>
+    
       <div class="flex flex-wrap justify-center">
         <form onSubmit={handleOrcSubmit}>
         <input  placeholder={"Type Orc Id here"} ref={orcLookupRef} />
@@ -85,24 +90,50 @@ return (
       </div>  
 {orcImage &&
 (
-<div ref={ref} style={{imageRendering: "pixelated"}} class="p2">
+<div class="flex flex-wrap">
+<div style={{imageRendering: "pixelated"}} class="p2 w-1/3">
 
-<svg class="rounded-full" id="orc" width={300} height={300} version="1.1" viewBox="13 3 30 40" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+<svg ref={ref0} class="rounded-full" id="orc" width={300} height={300} version="1.1" viewBox="13 3 30 40" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[1]}`}/>
     <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[2]}`}/>
   
 </svg>
 
+<div class="flex justify-evenly pt-4">
+<button onClick={()=>{onButtonClick(0)}}>Download</button>
+</div>
+</div>
+<div style={{imageRendering: "pixelated"}} class="p2 w-1/3">
+
+<svg ref={ref1} class="rounded-full" id="orc" width={300} height={300} version="1.1" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[1]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[2]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[3]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[4]}`}/>
+</svg>
+
+<div class="flex justify-evenly pt-4">
+<button onClick={()=>{onButtonClick(1)}}>Download</button>
+</div>
+</div>
+<div style={{imageRendering: "pixelated"}} class="p2 w-1/3">
+
+<svg ref={ref2} id="orc" width={300} height={300} version="1.1" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[1]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[2]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[3]}`}/>
+    <image x="1" y="1" width="60" height="60" imageRendering="pixelated" preserveAspectRatio="xMidYMid" xlinkHref={`${orcImage[4]}`}/>  
+</svg>
+
+<div class="flex justify-evenly pt-4">
+<button onClick={()=>{onButtonClick(2)}}>Download</button>
+</div>
 
 </div>
-)}
-
-{orcImage &&(
-
-<div class="flex justify-evenly">
-<button onClick={onButtonClick}>Download PFP</button>
 </div>
+
 )}
+
 
 </>
   )
